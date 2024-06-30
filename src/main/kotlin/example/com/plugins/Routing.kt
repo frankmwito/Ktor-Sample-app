3063eb28f21b7c6f03e6f65c0e4836c504a765ea
Frank Mwito <Mfranklyne039@gmail.com>
2024-06-30 23:20:27 +0300

    Ktor server side - Http requests and endpoints

--- src/main/kotlin/example/com/plugins/Routing.kt
+++ src/main/kotlin/example/com/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package example.com.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.response.*
@@ -10,5 +11,11 @@ fun Application.configureRouting() {
         get("/") {
             call.respondText("wassup")
         }
+        get("/test1") {
+            val text = "<h1>Hello From Ktor</h1>"
+            val type = ContentType.parse("text/html")
+            call.respondText(text, type)
+        }
+
     }
 }
